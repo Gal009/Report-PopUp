b07d5a66877409f2357ca0d09176535ed7d214f8
Gal009 <92556891+Gal009@users.noreply.github.com>
2022-01-04 15:27:26 -0300

    Initial Release

---
+++ PopUpPlugin/Config.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace PopUpPlugin
+{
+    public sealed class Config : IConfig
+    {
+
+        [Description("Se o plugin ta habilitado")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("A mensagem que aparece no poput")]
+        public string JoinedServerMessage { get; set; } = "Bem vindo ao servidor de jogos do zEscurera";
+    }
+}
---
+++ PopUpPlugin/Handlers/Player2.cs
@@ -0,0 +1,18 @@
+﻿using Exiled.Events.EventArgs;
+using CustomPlayerEffects;
+using Exiled.API.Enums;
+
+namespace PopUpPlugin.Handlers
+{
+    public class Player2
+    {
+
+        private readonly Plugin plugin;
+        public Player2(Plugin plugin) => this.plugin = plugin;
+
+        public void OnPlayerVerified(VerifiedEventArgs ev)
+        {
+            ev.Player.OpenReportWindow(plugin.Config.JoinedServerMessage);
+        }
+    }
+}
